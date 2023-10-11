f1eb8e2e99b0ba3fe8e5b3e5fd30c85f7a0bb474
afeezdev <afeezdev@gmail.com>
2023-10-11 17:50:18 +0100

    resize and rotate

--- images.py
+++ images.py
@@ -5,6 +5,12 @@ img = Image.open('./Pokedex/pikachu.jpg')
 # filtered_img.save("blur.png", 'png')
 filtered_img = img.convert('L')
 
+crooked = filtered_img.rotate(90)
+resize = filtered_img.resize((300,300))
+
 filtered_img.save("grey.png", 'png')
+crooked.save("crooked.png", "png")
+resize.save('resize.png','png')
+resize.show()
 
 
