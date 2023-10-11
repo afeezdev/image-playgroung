4e4756febff7c4349149d8562e51da9ccdc81248
afeezdev <afeezdev@gmail.com>
2023-10-11 18:35:22 +0100

    thumbnail

--- images.py
+++ images.py
@@ -1,11 +1,8 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.convert('L')
-box = (100, 100, 400, 400)
-region = filtered_img.crop(box)
-
-region.save('cropped.png','png')
+img = Image.open('./astro.jpg')
+img.thumbnail((400, 400))
+img.save('thumnails.jpg')
 
 
 
