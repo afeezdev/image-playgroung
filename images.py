170be172b6e0f38e906412e564177d4d6c24d4e3
afeezdev <afeezdev@gmail.com>
2023-10-11 18:04:51 +0100

    cropped

--- images.py
+++ images.py
@@ -1,16 +1,11 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
-# filtered_img = img.filter(ImageFilter.BLUR)
-# filtered_img.save("blur.png", 'png')
 filtered_img = img.convert('L')
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
 
-crooked = filtered_img.rotate(90)
-resize = filtered_img.resize((300,300))
+region.save('cropped.png','png')
 
-filtered_img.save("grey.png", 'png')
-crooked.save("crooked.png", "png")
-resize.save('resize.png','png')
-resize.show()
 
 
