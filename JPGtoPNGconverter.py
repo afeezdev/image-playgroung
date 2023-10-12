1e9c66b84fa21b309423d62d219a14d6cb197a0c
afeezdev <afeezdev@gmail.com>
2023-10-12 17:01:54 +0100

    JPGtoPNGconvert

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+
+# check if new/ exists, if not create it
+
+# Loop through Pokedex,
+# convert images to png
+# save them to the new folder
--- images.py
+++ images.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageFilter
 img = Image.open('./astro.jpg')
 img.thumbnail((400, 400))
 img.save('thumnails.jpg')
+print(img.size)
 
 
 
