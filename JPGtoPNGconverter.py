a6a59d5a9409bf9bb08cc1dec55477666276f351
afeezdev <afeezdev@gmail.com>
2023-10-14 01:38:23 +0100

    clean name

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -3,9 +3,23 @@ import os
 from PIL import Image
 
 # grab first and second argument
-
 # check if new/ exists, if not create it
-
 # Loop through Pokedex,
 # convert images to png
 # save them to the new folder
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)
+    img.save(f'{output_folder}{clean_name[0]}.png', 'png')
+    print('all done!')
+
+
+# Run in terminal with "python3 JPGtoPNGconverter.py Pokedex/ new/ "
